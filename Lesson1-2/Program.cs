70b48046ce1369732141ea56fbfe6a992eaa6367
zamotin-aa <zamotin.info@gmail.com>
2023-07-30 12:51:49 +0300

    gitignore

---
+++ Lesson1-2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Console.WriteLine("введите ваше имя");
+//string username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);
+
+
+Console.Write("введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
