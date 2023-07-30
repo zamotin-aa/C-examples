4b1db9957e8e87cc60a0d35ff2492fb20a5669d3
zamotin-aa <zamotin.info@gmail.com>
2023-07-30 13:25:14 +0300

    Example6

---
+++ Lesson1-5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username= Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
