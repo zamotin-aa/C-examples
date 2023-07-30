9153b07711b89615aa6ee8538844a73375bbe7fc
zamotin-aa <zamotin.info@gmail.com>
2023-07-30 13:29:23 +0300

    Example7

---
+++ Lesson1-6/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1;
+int b = 6;
+int c = 4;
+int d = 7;
+int e = 2;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
