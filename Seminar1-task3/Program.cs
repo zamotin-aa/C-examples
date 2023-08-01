e92e5761cf23923a9e5050bb4b971a355e3c9730
zamotin-aa <zamotin.info@gmail.com>
2023-08-01 21:43:49 +0300

    Семинар 1, задача 3 второй вариант

--- Seminar1-task3/Program.cs
+++ Seminar1-task3/Program.cs
@@ -32,12 +32,39 @@ if (input1 == 7)
 {
     Console.WriteLine("Воскресенье");
 }
-if (input1 > 7) 
+if (input1 > 7 || input1 < 1) 
 {
     Console.WriteLine("Такого дня нет");
 }
 
+//Вариант 2
 
+//Console.WriteLine("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number== 1){
+//     Console.WriteLine("Понедельник");
+//}
+//else if  (number== 2){
+//    Console.WriteLine("Вторник");
+//}
+//else if  (number== 3){
+//     Console.WriteLine("Среда");
+//}
+//else if  (number== 4){
+//     Console.WriteLine("Четверг");
+//} 
+//else if  (number== 5){
+//     Console.WriteLine("Пятница");
+//}
+//else if  (number== 6){
+//     Console.WriteLine("Суббота");
+//}
+// else if  (number== 7){
+//     Console.WriteLine("Воскресенье");
+//} 
+//else {
+//    Console.WriteLine("Введи от 1 до 7");
+//}
 
 
 
