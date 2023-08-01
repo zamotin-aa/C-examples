524adbac756a56f34a767c83ba3de16ae38454f8
zamotin-aa <zamotin.info@gmail.com>
2023-08-01 20:44:07 +0300

    Sem1 task1

---
+++ Seminar1-task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число и 
+//выдает его квадрат (число умноженное на само себя).
+//Например:
+//4 -> 16 
+
+//string input = Console.ReadLine();
+//int number = Convert.ToInt32(input);
+//вместо этого можно записать в одну строку (см.ниже)
+
+Console.WriteLine("Введите число");
+int input = Convert.ToInt32(Console.ReadLine());
+int sqr_number = input * input;
+Console.WriteLine("Наш результат - " + sqr_number);
+
+
