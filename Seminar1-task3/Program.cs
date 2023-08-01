9d16e771a6518bff3563bdae7e0537ab7324e8d5
zamotin-aa <zamotin.info@gmail.com>
2023-08-01 21:22:59 +0300

    Семинар 1, задача 2-3

---
+++ Seminar1-task3/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда 
+//5 -> Пятница
+
+Console.WriteLine("Введите номер дня недели");
+int input1 = Convert.ToInt32(Console.ReadLine());
+if (input1 == 1) 
+{
+    Console.WriteLine("Понедельник");
+}
+if (input1 == 2) 
+{
+    Console.WriteLine("Вторник");
+}
+if (input1 == 3) 
+{
+    Console.WriteLine("Среда");
+}
+if (input1 == 4) 
+{
+    Console.WriteLine("Четверг");
+}
+if (input1 == 5) 
+{
+    Console.WriteLine("Пятница");
+}
+if (input1 == 6) 
+{
+    Console.WriteLine("Суббота");
+}
+if (input1 == 7) 
+{
+    Console.WriteLine("Воскресенье");
+}
+if (input1 > 7) 
+{
+    Console.WriteLine("Такого дня нет");
+}
+
+
+
+
+
+
