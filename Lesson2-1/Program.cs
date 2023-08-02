0d00d38b936abb080a9a1f9a6535335f044fece4
zamotin-aa <zamotin.info@gmail.com>
2023-08-02 23:45:16 +0300

    Lesson 2

---
+++ Lesson2-1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//запись массива
+
+// int[] array= {1,4,5,634,23,4,44} ;
+// int max = array[1];
+// Console.WriteLine(max);
+
+
+//поиск номера в массиве
+int[] array = {1,4,5,634,23,4,44};
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
+
---
+++ Lesson2-2/Program.cs
@@ -0,0 +1,54 @@
+﻿void FillArray(int[] collection) //метод сгенерировать массив
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+//void ключевое слово показывает что метод ничего не возвращает
+
+void PrintArray(int[] col) //метод вывести на печать массив
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) //метод, найти индекс по заданному значению массива
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; 
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+//создай массив на 10 элементов, по умолчанию запишутся 0000
+
+//вызвали методы заполнения и печати
+FillArray(array); //заполнили массив
+PrintArray(array); //вывели на печать
+
+Console.WriteLine("");
+
+int pos = IndexOf(array, 4); //вызвали метод находжения позиции
+
+Console.WriteLine(pos);
