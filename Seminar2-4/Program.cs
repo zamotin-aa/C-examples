43fa50734d84be0b5adf3c64cfd89ef9096d7dcc
zamotin-aa <zamotin.info@gmail.com>
2023-08-04 21:22:26 +0300

    Семинар 2 второй блок задач дополнение

--- Seminar2-4/Program.cs
+++ Seminar2-4/Program.cs
@@ -21,7 +21,17 @@ else if (a != 0)
     Console.WriteLine("не кратно, остаток - " + a);
 }
 
+// Второй вариант
 
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 % number2 == 0){
+    Console.WriteLine("Кратно");
+}
+else{
+    Console.WriteLine("Не кратно");
+    Console.WriteLine($"остаток = {number1 % number2}");
+}
 
 
 
