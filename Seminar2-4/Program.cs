962281dba05fbf90f81bc0d25e3ff4346a6bc25f
zamotin-aa <zamotin.info@gmail.com>
2023-08-04 21:45:34 +0300

    Seminar 2 complete

--- Seminar2-4/Program.cs
+++ Seminar2-4/Program.cs
@@ -1,9 +1,7 @@
 ﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
 // является ли первое число кратным второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.
-
 // 34, 5 -> не кратно, остаток 4 
 // 16, 4 -> кратно
-
 
 Console.WriteLine("Введите первое число");
 int input1 = Convert.ToInt32(Console.ReadLine());
@@ -16,22 +14,20 @@ if (a == 0)
 {
     Console.WriteLine("Кратно");
 }
-else if (a != 0)
+else //if (a != 0) не обязательно
 {
     Console.WriteLine("не кратно, остаток - " + a);
 }
-
 // Второй вариант
-
-int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = Convert.ToInt32(Console.ReadLine());
-if(number1 % number2 == 0){
-    Console.WriteLine("Кратно");
-}
-else{
-    Console.WriteLine("Не кратно");
-    Console.WriteLine($"остаток = {number1 % number2}");
-}
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if(number1 % number2 == 0){
+//     Console.WriteLine("Кратно");
+// }
+// else{
+//     Console.WriteLine("Не кратно");
+//     Console.WriteLine($"остаток = {number1 % number2}");
+// }
 
 
 
