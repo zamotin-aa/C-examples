3599b1aea2f94595a272a669322dc5c2fef25ca5
zamotin-aa <zamotin.info@gmail.com>
2023-08-22 20:53:52 +0300

    Третий семинар1

---
+++ Seminar3-1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Заданы координаты - вывести в какой четверти находимся
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+// создать массив на два элемента
+int[] array = new int[2];
+array[0] = x;
+array[1] = y;
+
+// можно заполнить циклом
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("2");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
+else
+{
+    Console.WriteLine("Есть нулевые координаты");
+}
---
+++ Seminar3-2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите координаты");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Возможные координаты x");
+
+if (x > 0)
+{
+    for (int i = 0; i <= x; i++)
+        Console.Write(i + " ");
+}
+else
+{
+    for (int i = 0; i >= x; i--)
+        Console.Write(i + " ");
+}
+
+Console.WriteLine("\n" + "Возможные координаты y");
+
+if (y > 0)
+{
+    for (int i = 0; i <= y; i++)
+        Console.Write(i + " ");
+}
+else
+{
+    for (int i = 0; i >= y; i--)
+        Console.Write(i + " ");
+}
