feb6d5173bb051077a1fee66dbbb86b111e6bd26
zamotin-aa <zamotin.info@gmail.com>
2023-08-22 21:28:02 +0300

    третья задача, третий семинар

--- Seminar3-2/Program.cs
+++ Seminar3-2/Program.cs
@@ -30,3 +30,35 @@ else
     for (int i = 0; i >= y; i--)
         Console.Write(i + " ");
 }
+
+
+// Второй вариант
+
+// Console.WriteLine("Введите четветь (от 1 до 4): ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+// if (num == 1) 
+// {
+//     Console.WriteLine($"Диапазон X от {1} до {int.MaxValue} ");
+//     Console.WriteLine($"Диапазон Y от {1} до {int.MaxValue} ");
+
+// }
+// else if (num == 2) 
+// {
+//     Console.WriteLine($"Диапазон X от {- 1} до {int.MinValue} ");
+//     Console.WriteLine($"Диапазон Y от {1} до {int.MaxValue} ");
+
+// }
+// else if (num == 3) 
+// {
+//     Console.WriteLine($"Диапазон X от {- 1} до {int.MinValue} ");
+//     Console.WriteLine($"Диапазон Y от {- 1} до {int.MinValue} ");
+
+// }
+// else if (num == 4) 
+// {
+//     Console.WriteLine($"Диапазон X от {1} до {int.MaxValue} ");
+//     Console.WriteLine($"Диапазон Y от {- 1} до {int.MinValue} ");
+
+// }
---
+++ Seminar3-4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Вычисление расстояния между взятыми на плоскости двумя точками А(хА; уА) и В(хВ; уВ), 
+// выполняется по формуле d = √((хА – хВ)2 + (уА – уВ)2)
+
+// Math.Pow(число которое возводим в степень, значение степени)
+// Math.Sqrt (число) - взятие квадратного корня из числа
+
+Console.WriteLine("Введите координаты первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Pow(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2), 0.5);
+
+// double d = Math.Pow((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)), 0.5);
+
+// double d = Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+
+double d = Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)), 2); //с округлением
+
+Console.WriteLine(d);
+
+
+
