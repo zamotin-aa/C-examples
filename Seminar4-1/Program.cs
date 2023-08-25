693137e03c3dab5ee5b0f7737f9de8332abcf55c
zamotin-aa <zamotin.info@gmail.com>
2023-08-25 21:04:21 +0300

    семинар 4 задача 1

---
+++ Seminar4-1/Program.cs
@@ -0,0 +1,53 @@
+﻿
+// Задача 24: Напишите программу, которая принимает на 
+// вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+// for (int i = 0; i <= number; i++)
+// {
+//     sum += i;
+// }
+// Console.WriteLine(sum);
+
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Parse(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Parse("Введите число");
+
+int Sumx(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+
+    int count = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        count += 1;
+    }
+    return count;
+}
+
+Console.WriteLine(Sumx(number));
+
+
+
+
+
---
+++ Seminar4-2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход 
+// число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int Parse(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Parse("Введите число");
+
+int Sumxx(int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
+
+Console.WriteLine(Sumxx(number));
